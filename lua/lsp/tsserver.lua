1d6ded77155460138da0e173e2ecbe26a715d356
Slava Rykov <v.rykov@lamoda.ru>
2025-06-18 16:47:15 +0500

    change ts and vue lsp config

--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -1,3 +1,16 @@
 local lspconfig = require("lspconfig")
 
-lspconfig.ts_ls.setup({})
+local volar_ts_plugin_path = vim.fn.stdpath("data") .. "/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin";
+
+lspconfig.ts_ls.setup({
+	init_options = {
+		plugins = {
+			{
+				name = "@vue/typescript-plugin",
+				location = volar_ts_plugin_path,
+				languages = { "vue" },
+			},
+		},
+	},
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+})
--- lua/lsp/vueserver.lua
+++ lua/lsp/vueserver.lua
@@ -3,7 +3,7 @@ local lspconfig = require("lspconfig")
 lspconfig.volar.setup({
 	init_options = {
 		vue = {
-			hybridMode = false,
+			hybridMode = true,
 		},
 	},
 })
