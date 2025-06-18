8d27a9f327b5d5a93bf2711c9f1d2c44adacccf7
Slava Rykov <v.rykov@lamoda.ru>
2025-06-18 16:59:09 +0500

    remove semicolon

--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -1,6 +1,6 @@
 local lspconfig = require("lspconfig")
 
-local volar_ts_plugin_path = vim.fn.stdpath("data") .. "/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin";
+local volar_ts_plugin_path = vim.fn.stdpath("data") .. "/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
 
 lspconfig.ts_ls.setup({
 	init_options = {
